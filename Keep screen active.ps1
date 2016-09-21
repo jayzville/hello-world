8abe952011829e6e3a4f9bcc39cb5021f233c328
jayzville <jayzville@xtra.co.nz>
2016-09-21 14:53:19 +1200

    Keeping the Screen Active
    Testing Files

---
+++ Keep screen active.ps1
@@ -0,0 +1,10 @@
+﻿param($minutes = 60)
+
+$myshell = New-Object -com "Wscript.Shell"
+
+for ($i = 0; $i -lt $minutes; $i++) {
+  Start-Sleep -Seconds 60
+  $myshell.sendkeys(" ")
+}
+$Pos = [System.Windows.Forms.Cursor]::Position
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) + 1) , $Pos.Y)
